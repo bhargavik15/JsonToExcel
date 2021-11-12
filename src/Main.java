487831d741df2814b58fe126fc3853f04be6b4df
bhargavik15 <bhargavi15419@gmail.com>
2021-11-12 18:44:48 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        LogicClass log = new LogicClass();
+        log.readTheJsonFile();
+        log.generateExcelFile();
+    }
+}
